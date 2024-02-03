9a62452b2ad766656c6bbb5e54c29f70c94ad6bb
Shouki SAKAMOTO <32442796+shouki-s@users.noreply.github.com>
2024-02-03 15:43:19 +0900

    moment and age

--- components/SectionMain.vue
+++ components/SectionMain.vue
@@ -3,11 +3,24 @@
     <NavBar />
     <div class="container my-5">
       <h2 id="profile">プロフィール</h2>
+      <p>
+        <ruby>坂本<rp>（</rp><rt>さかもと</rt><rp>）</rp></ruby>&nbsp;
+        <ruby>鐘期<rp>（</rp><rt>しょうき</rt><rp>）</rp></ruby>。
+        1984年生まれ、{{ age }}歳。
+      </p>
       <h2>スキル</h2>
       <h2>連絡先</h2>
     </div>
   </main>
 </template>
+
+<script setup lang="ts">
+import moment from 'moment'
+import { ref } from 'vue'
+
+const diff = moment().diff(moment('1984-297T00:00:00+09:00'))
+const age = ref(moment.duration(diff).years())
+</script>
 
 <style lang="scss" scoped>
 h2 {
