2010bc4d1f03b587023ef5b26151337df3d5e417
Shouki SAKAMOTO <32442796+shouki-s@users.noreply.github.com>
2024-02-03 16:08:48 +0900

    refactor footer

--- app.vue
+++ app.vue
@@ -1,7 +1,5 @@
 <template>
   <SectionTop />
   <SectionMain />
-  <footer class="bg-body-secondary" style="height: 10em">
-    &copy; Shouki SAKAMOTO.
-  </footer>
+  <SectionFooter />
 </template>
---
+++ components/SectionFooter.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer
+    class="text-secondary-emphasis bg-body-secondary d-flex justify-content-center align-items-center"
+  >
+    &copy; 2024 Shouki SAKAMOTO
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+footer {
+  height: 4em;
+}
+</style>
