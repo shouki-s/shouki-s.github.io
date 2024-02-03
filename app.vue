0e4305b6362c6ac9855a6a3e128834e0e8de7500
Shouki SAKAMOTO <32442796+shouki-s@users.noreply.github.com>
2024-02-03 15:36:51 +0900

    refactor

--- app.vue
+++ app.vue
@@ -1,23 +1,7 @@
 <template>
   <SectionTop />
-  <section class="text-section">
-    <NavBar />
-    <div class="container my-5">
-      <h2 id="profile">プロフィール</h2>
-      <h2>スキル</h2>
-      <h2>連絡先</h2>
-    </div>
-  </section>
+  <SectionMain />
   <footer class="bg-body-secondary" style="height: 10em">
     &copy; Shouki SAKAMOTO.
   </footer>
 </template>
-
-<style lang="scss" scoped>
-h2 {
-  padding-top: 3em;
-}
-.text-section {
-  min-height: 100dvh;
-}
-</style>
---
+++ components/SectionMain.vue
@@ -0,0 +1,19 @@
+<template>
+  <main class="text-section">
+    <NavBar />
+    <div class="container my-5">
+      <h2 id="profile">プロフィール</h2>
+      <h2>スキル</h2>
+      <h2>連絡先</h2>
+    </div>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+h2 {
+  padding-top: 3em;
+}
+.text-section {
+  min-height: 100dvh;
+}
+</style>
