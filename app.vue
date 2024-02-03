674ad06c3ecbc0790f1f34b0c1b72d5b1c48fc2b
Shouki SAKAMOTO <32442796+shouki-s@users.noreply.github.com>
2024-02-03 15:03:51 +0900

    delay

--- app.vue
+++ app.vue
@@ -3,9 +3,13 @@
     <section
       class="bc-section d-flex flex-column justify-content-center align-items-center position-relative"
     >
-      <a href="#profile" class="button-next btn btn-secondary">
-        <i class="bi bi-chevron-double-down" />
-      </a>
+      <div
+        class="button-next-wrapper animate__animated animate__fadeIn animate__delay-2s"
+      >
+        <a href="#profile" class="button-next btn btn-secondary">
+          <i class="bi bi-chevron-double-down" />
+        </a>
+      </div>
       <BusinessCard />
     </section>
     <section class="text-section">
@@ -30,17 +34,32 @@ h2 {
   width: 100dvw;
   height: 100dvh;
 }
-.button-next {
+.button-next-wrapper {
   position: absolute;
-  left: 50%;
-  transform: translateX(-50%);
-  bottom: 12px;
+  left: auto;
+  right: auto;
+  bottom: 16px;
+}
+.button-next {
   border-radius: 50%;
   font-size: 48px;
   line-height: 1;
   aspect-ratio: 1;
   display: inline-flex;
   align-items: center;
+
+  animation: fadeIn ease both;
+  animation-timeline: scroll();
+  animation-range: 0 100dvh;
+
+  @keyframes fadeIn {
+    from {
+      opacity: 1;
+    }
+    to {
+      opacity: 0;
+    }
+  }
 }
 .text-section {
   min-height: 100dvh;
